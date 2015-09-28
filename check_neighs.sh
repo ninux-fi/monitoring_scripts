22adfd5a49b066437ca2fa1b0595d9a3f8fbc1e6
leonardo <leonardo.maccari@unitn.it>
2015-09-28 22:49:56 +0200

    debugged script for busybox

--- check_neighs.sh
+++ check_neighs.sh
@@ -1,17 +1,21 @@
-
-
 #!/bin/sh
 
 NEIGHS=/tmp/neighs.txt
 PINGS=10
-PINGFOLDER=/tmp/www/ping
+PINGFOLDER=/www/ping
 PINGFILE=$PINGFOLDER/ping.csv
-TXTINFO_URL="http://tetto.bellanzer2.fi.nnx:2006/links"
+PINGFILE_BAK=/tmp/ping.csv.bak
+TXTINFO_URL="http://localhost:2006/links"
+MAX_LINES=1000
 
 mkdir -p $PINGFOLDER
 
-curl $TXTINFO_URL > $NEIGHS
+#curl $TXTINFO_URL > $NEIGHS
+wget -q -O $NEIGHS $TXTINFO_URL 
 NOW=`date +"%Y-%m-%d %H:%H"`
+
+tail -n $MAX_LINES $PINGFILE > $PINGFILE_BAK
+mv $PINGFILE_BAK $PINGFILE
 
 LINKS=`cat $NEIGHS | grep -v IP | grep -v "Table"`;
 IFS=$'\n'
@@ -20,9 +24,8 @@ for link in $LINKS; do
  SIP=`echo $link |  cut -f 1`
  DIP=`echo $link |  cut -f 2`
  echo -n "$NOW $SIP $DIP" >> $PINGFILE;
- ping -c $PINGS -q $DIP | grep "mdev" | cut -d= -f 2 | sed "s/\// /g" | sed "s/ms//g" >> $PINGFILE
+ ping -c $PINGS -q $DIP | grep "max" | cut -d= -f 2 | sed "s/\// /g" | sed "s/ms//g" >> $PINGFILE
 done;
 IFS=$' '
 
 rm $NEIGHS
-
