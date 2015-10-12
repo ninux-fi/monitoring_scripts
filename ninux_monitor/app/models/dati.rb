b472bf79ad8dad6002c3c0954827c5032532ff35
Alessio Caiazza <nolith@abisso.org>
2015-10-12 18:56:36 +0200

    relazione nodo dati

--- ninux_monitor/app/models/dati.rb
+++ ninux_monitor/app/models/dati.rb
@@ -2,6 +2,8 @@ class Dati < ActiveRecord::Base
   self.primary_key = 'ID'
   self.table_name = 'dati'
 
+  belongs_to :nodo, class_name: 'Nodi', foreign_key: 'id_nodo', primary_key: "ID"
+
   def quando
     "#{self.giorno} #{self.mese} #{self.anno} - #{self.ora_remota}"
   end
--- ninux_monitor/app/models/nodi.rb
+++ ninux_monitor/app/models/nodi.rb
@@ -1,4 +1,7 @@
 class Nodi < ActiveRecord::Base
   self.primary_key = 'ID'
   self.table_name = 'nodi'
+
+  has_many :dati, class_name: 'Dati', foreign_key: 'id_nodo', primary_key: "ID"
+
 end
