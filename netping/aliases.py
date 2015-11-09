56d4aabb611dc8ad34a253cd40622095db36d6e6
leonardo <leonardo.maccari@unitn.it>
2015-11-09 22:20:44 +0100

    PEP-8 compliant

--- netping/aliases.py
+++ netping/aliases.py
@@ -4,24 +4,24 @@ import urllib2
 import json
 import sys
 
+
 class olsr_graph():
 
-    def __init__(self,json_url):
-        self.json_url=json_url
-        
-    def get_json(self):
-        resp=urllib2.urlopen(self.json_url)
-        f=resp.read()
-        f_json=json.loads(f)
-        invert_repo={}
+    def __init__(self, json_url):
+        self.json_url = json_url
+
+    def get_mid(self):
+        resp = urllib2.urlopen(self.json_url)
+        f = resp.read()
+        f_json = json.loads(f)
+        invert_repo = {}
         for address in f_json["mid"]:
-            invert_repo[address["ipAddress"]]=[]
+            invert_repo[address["ipAddress"]] = []
             for aliases in address["aliases"]:
                 invert_repo[address["ipAddress"]].append(aliases["ipAddress"])
-            print address["ipAddress"],invert_repo[address["ipAddress"]]
+            print address["ipAddress"], invert_repo[address["ipAddress"]]
 
-url=sys.argv[1]
-c=olsr_graph(url)
-c.get_json()
 
-                
+url = sys.argv[1]
+c = olsr_graph(url)
+c.get_mid()
